835e3e792562b18b6f0446b5007533dd947a2472
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-01-19 18:07:45 +0330

    when click on enter add item

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,5 +1,41 @@
+const spanOne = document.querySelector('.span-one span');
+const todoList = document.querySelector('.content ul');
+spanOne.innerText = document.querySelectorAll('.list-item input[type="checkbox"]').length;
+
 //build change theme when click
 document.querySelector('#inputTheme').addEventListener('click' , () => {
     document.querySelector('body').classList = [inputTheme.checked ? 'theme-light' : 'theme-dark']
 });
 
+//add item for list
+const addNewItem = document.querySelector('#addItem');
+addNewItem.addEventListener('keypress' , (event)=>{
+    if(event.key === "Enter" && addNewItem.value !== ""){
+        newItemFunc(addNewItem.value);
+        addNewItem.value = '';
+    }
+});
+
+function newItemFunc(input) {
+    const newLI = document.createElement('li');
+    newLI.classList.add('contineir');
+    
+    newLI.innerHTML = `
+        <label class="list-item">
+        <input type="checkbox" name="todoItem">
+        <span class="checkmark"></span>
+        <span class="input">${input}</span>
+    </label>
+    <span class="remove"></span>
+    `;
+    
+    if (document.querySelector('.filter input[type="radio"]:checked').id === 'completed') {
+        newLI.classList.add('hidden');
+    }
+    todoList.append(newLI);
+    updateItemes(1);
+}
+
+function updateItemes(number) {
+    spanOne.innerText = +spanOne.innerText + number;
+}
